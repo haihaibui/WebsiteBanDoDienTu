53bdfbb560760e9be6f99836df11bf3c5c58cbfd
Hoang0207 <giuse02072003@gmail.com>
2024-10-24 11:02:29 +0700

    hoàn thiện chức năng quản lý chủng loại
    - thêm chủng loại
    - sửa chung loại
    - xóa chủng loại

--- src/main/resources/static/admin/js/QuanLyChungLoai.js
+++ src/main/resources/static/admin/js/QuanLyChungLoai.js
@@ -1,39 +1,101 @@
-app.controller('QuanLyChungloaiCtrl',function($http, $scope){
-	
+app.controller('QuanLyChungloaiCtrl', function($http, $scope) {
+
 	$scope.form = {}
 	$scope.items = []
-	$scope.work_type = "ThemMoi"
-	
+	$scope.update = false
+
 	//reset lại form
-	$scope.reset = function(){
+	$scope.reset = function() {
 		$scope.form = {}
-		$scope.work_type = "ThemMoi"
+		$scope.update = false
 	}
-	
+
 	//load lên toàn bộ bảng chủng loại
-	$scope.load_all = function(){
+	$scope.load_all = function() {
 		var url = `${host}/ChungLoai`
-		$http.get(url).then(resp =>{
+		$http.get(url).then(resp => {
 			$scope.items = resp.data;
 			console.log("Get all ChungLoai  success")
-		}).catch(error =>{
+		}).catch(error => {
 			console.log("error get all ChungLoai", error)
 		})
 	}
-	
+
 	//Hiện chi tiết chủng loại theo id
-	$scope.load_by_id = function(maCl){
+	$scope.load_by_id = function(maCl) {
 		var url = `${host}/ChungLoai/${maCl}`
 		$http.get(url).then(resp => {
 			$scope.form = resp.data
-			$scope.work_type = "CapNhat"
-			console.log("Success edit ChungLoai by id",resp)
+			$scope.update = true
+			console.log("Success edit ChungLoai by id", resp)
 		}).catch(error => {
-			console.log("Error edit ChungLoai by id",error)
+			console.log("Error edit ChungLoai by id", error)
 		})
 	}
-	
+
+	//Thêm chủng loại mới
+	$scope.create = function() {
+		var url = `${host}/ChungLoai`
+		$http.post(url, $scope.form).then(resp => {
+			$scope.load_all()
+			$scope.update = true
+			swal({
+				title: "Thêm thành công !",
+				text: "Bạn đã thêm thành công chủng loại mới",
+				icon: "success",
+				button: "Đóng",
+			});
+			console.log("Success create ChungLoai", resp)
+		}).catch(error => {
+			console.log("Error create ChugnLoai", error)
+		})
+	}
+
+	//Cập nhật chủng loại
+	$scope.update_cl = function(macl) {
+		var url = `${host}/ChungLoai/${macl}`
+		$http.put(url, $scope.form).then(resp => {
+			$scope.load_all()
+			swal({
+				title: "Cập nhật thành công !",
+				text: "Bạn đã cập nhật thành công chủng loại",
+				icon: "success",
+				button: "Đóng",
+			});
+			console.log("Success update ChungLoai", resp)
+		}).catch(error => {
+			console.log("Error update ChungLoai", error)
+		})
+	}
+
+	//Xóa chủng loại
+	$scope.delete = function(macl) {
+		swal({
+			title: "Bạn chắc chắn không ?",
+			text: "Khi đã xóa thì bạn không thể khôi phục lại được",
+			icon: "warning",
+			buttons: true,
+			dangerMode: true,
+		})
+			.then((willDelete) => {
+				if (willDelete) {
+					var url = `${host}/ChungLoai/${macl}`
+					$http.delete(url).then(resp => {
+						$scope.load_all()
+						console.log("Success delete ChungLoai")
+						swal("Xóa thành công", {
+							icon: "success",
+						});
+					}).catch(error => {
+						console.log("Error delete ChungLoai", error)
+					})
+				} else {
+					swal("Không thực hiện thao tác xóa");
+				}
+			});
+	}
+
 	//Tự động chạy đầu tiên khi mở tab Quản lý chủng loại
 	$scope.load_all()
-	
+
 })
