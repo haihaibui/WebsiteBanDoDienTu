d66e5992ed55e06762e38014d78700f0fcb48c01
Hoang0207 <giuse02072003@gmail.com>
2024-10-25 22:48:28 +0700

    Xem chi tiết sản phẩm

--- src/main/resources/static/admin/js/QuanLySanPham.js
+++ src/main/resources/static/admin/js/QuanLySanPham.js
@@ -2,6 +2,20 @@ app.controller('QuanLySanPhamCtrl',function($http, $scope){
 	
 	$scope.items = []
 	
+	$scope.list_cl = []
+	
+	$scope.list_ncc = []
+	
+	$scope.list_ttdb = []
+	
+	$scope.update = false;
+	
+	//Reset lại form
+	$scope.reset = function(){
+		$scope.form = {}
+		$scope.update = false
+	}
+	
 	//Load tất cả sản phẩm
 	$scope.load_all = function(){
 		var url = `${host}/SanPham`
@@ -13,7 +27,51 @@ app.controller('QuanLySanPhamCtrl',function($http, $scope){
 		})
 	}
 	
+	//Load san pham theo id
+	$scope.load_by_id = function(maSp){
+		var url = `${host}/SanPham/${maSp}`
+		$http.get(url).then(resp => {
+			$scope.form = resp.data
+			$scope.update = true;
+		}).catch(error => {
+			console.log("Error load SanPham By Id", error)
+		})
+	}
+	
+	//Load danh sách chủng loại
+	$scope.load_list_cl = function(){
+		var url = `${host}/ChungLoai`
+		$http.get(url).then(resp => {
+			$scope.list_cl = resp.data
+		}).catch(error => {
+			console.log("Error load list Chung Loai", error)
+		})
+	}
+	
+	//Load danh sách nhà cung cấp 
+	$scope.load_list_ncc = function(){
+		var url = `${host}/NhaCungCap`
+		$http.get(url).then(resp => {
+			$scope.list_ncc = resp.data
+		}).catch(error => {
+			console.log("Error load list NhaCungCAp", error)
+		})
+	}
+	
+	//Load danh sách thuộc tính đặc biệt
+	$scope.load_list_ttdb = function(){
+		var url = `${host}/ThuocTinhDacBiet`
+		$http.get(url).then(resp => {
+			$scope.list_ttdb = resp.data
+		}).catch(error => {
+			console.log("Error load list TTDB",error)
+		})
+	}
+	
+	
 	//Tự động chạy khi mở tab
 	$scope.load_all()
-	
+	$scope.load_list_cl()
+	$scope.load_list_ncc()
+	$scope.load_list_ttdb()
 })
