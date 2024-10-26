9f79a8cb2fe5953ac4589decab049ba1c5b8a86f
Hoang0207 <giuse02072003@gmail.com>
2024-10-26 23:58:16 +0700

    Xử lý khi chọn hình ảnh cho modal sản phẩm

--- src/main/resources/static/admin/js/QuanLySanPham.js
+++ src/main/resources/static/admin/js/QuanLySanPham.js
@@ -1,45 +1,49 @@
-app.controller('QuanLySanPhamCtrl',function($http, $scope){
-	
+app.controller('QuanLySanPhamCtrl', function($http, $scope) {
+
 	$scope.items = []
-	
+
 	$scope.list_cl = []
-	
+
 	$scope.list_ncc = []
-	
+
 	$scope.list_ttdb = []
-	
+
 	$scope.update = false;
-	
+
+	$scope.default_image_src = "/api/file/SanPham/defaultProductPhoto.png"
+
 	//Reset lại form
-	$scope.reset = function(){
+	$scope.reset = function() {
 		$scope.form = {}
 		$scope.update = false
+		$scope.temp_image_src = null
 	}
-	
+
 	//Load tất cả sản phẩm
-	$scope.load_all = function(){
+	$scope.load_all = function() {
 		var url = `${host}/SanPham`
 		$http.get(url).then(resp => {
 			$scope.items = resp.data
 			console.log("Success load all SanPham")
 		}).catch(error => {
-			console.log("Error load all SanPham",error)
+			console.log("Error load all SanPham", error)
 		})
 	}
-	
+
 	//Load san pham theo id
-	$scope.load_by_id = function(maSp){
+	$scope.load_by_id = function(maSp) {
 		var url = `${host}/SanPham/${maSp}`
 		$http.get(url).then(resp => {
 			$scope.form = resp.data
+			$scope.temp_image_src = null
 			$scope.update = true;
 		}).catch(error => {
 			console.log("Error load SanPham By Id", error)
 		})
 	}
-	
+
 	//Load danh sách chủng loại
-	$scope.load_list_cl = function(){
+	$scope.load_list_cl = function() {
 		var url = `${host}/ChungLoai`
 		$http.get(url).then(resp => {
 			$scope.list_cl = resp.data
@@ -47,9 +51,9 @@ app.controller('QuanLySanPhamCtrl',function($http, $scope){
 			console.log("Error load list Chung Loai", error)
 		})
 	}
-	
+
 	//Load danh sách nhà cung cấp 
-	$scope.load_list_ncc = function(){
+	$scope.load_list_ncc = function() {
 		var url = `${host}/NhaCungCap`
 		$http.get(url).then(resp => {
 			$scope.list_ncc = resp.data
@@ -57,18 +61,36 @@ app.controller('QuanLySanPhamCtrl',function($http, $scope){
 			console.log("Error load list NhaCungCAp", error)
 		})
 	}
-	
+
 	//Load danh sách thuộc tính đặc biệt
-	$scope.load_list_ttdb = function(){
+	$scope.load_list_ttdb = function() {
 		var url = `${host}/ThuocTinhDacBiet`
 		$http.get(url).then(resp => {
 			$scope.list_ttdb = resp.data
 		}).catch(error => {
-			console.log("Error load list TTDB",error)
+			console.log("Error load list TTDB", error)
 		})
 	}
-	
-	
+
+	//Bắt sự kiện khi nhấp vào ảnh
+	$scope.image_click = function() {
+		document.getElementById('inputFile').click()
+	}
+
+	// Hiển thị ảnh đã chọn từ file
+	document.getElementById('inputFile').addEventListener('change', function(event) {
+		var input = event.target;
+		if (input.files && input.files[0]) {
+			var reader = new FileReader()
+			reader.onload = function(e) {
+				$scope.$apply(function() {
+					$scope.temp_image_src = e.target.result; // Cập nhật hiển thị tạm thời
+				})
+			}
+			reader.readAsDataURL(input.files[0]);
+		}
+	})
+
 	//Tự động chạy khi mở tab
 	$scope.load_all()
 	$scope.load_list_cl()
