d2db71e15f37bf406fd3d9477040a5977940e142
Hoang0207 <giuse02072003@gmail.com>
2024-10-26 10:13:53 +0700

    Xem chi tiết nhà cung cấp, xem chi tiết thuộc tính đặc biệt

--- src/main/resources/static/admin/js/QuanLyNhaCungCap.js
+++ src/main/resources/static/admin/js/QuanLyNhaCungCap.js
@@ -2,6 +2,15 @@ app.controller('QuanLyNhaCungCapCtrl',function($http, $scope){
 		
 	$scope.items = []
 	
+	$scope.update = false
+	
+	//Chức năng reset
+	$scope.reset = function(){
+		$scope.update = false
+		$scope.form = {}
+	}
+	
+	//load tất cả nhà cung cấp
 	$scope.load_all = function(){
 		var url = `${host}/NhaCungCap`
 		$http.get(url).then(resp =>{
@@ -9,6 +18,17 @@ app.controller('QuanLyNhaCungCapCtrl',function($http, $scope){
 			console.log("Success load all NhaCungCap")
 		}).catch(error =>{
 			console.log("Erorr load all NhaCungCap",error)
+		})
+	}
+	
+	//load nhà cung cấp theo id
+	$scope.load_by_id = function(maNcc){
+		var url = `${host}/NhaCungCap/${maNcc}`
+		$http.get(url).then(resp => {
+			$scope.form = resp.data
+			$scope.update = true
+		}).catch(error => {
+			console.log("Error load NhaCungCap by id", error)
 		})
 	}
 	
--- src/main/resources/static/admin/js/QuanLyThuocTinhDacBiet.js
+++ src/main/resources/static/admin/js/QuanLyThuocTinhDacBiet.js
@@ -2,6 +2,15 @@ app.controller('QuanLyThuocTinhDacBietCtrl',function($http, $scope){
 		
 	$scope.items = []
 	
+	$scope.update = false
+	
+	//Hàm reset
+	$scope.reset = function(){
+		$scope.form = {}
+		$scope.update = false
+	}
+	
+	//load tất cả thuộc tính đặc biệt
 	$scope.load_all = function(){
 		var url = `${host}/ThuocTinhDacBiet`
 		$http.get(url).then(resp => {
@@ -11,6 +20,18 @@ app.controller('QuanLyThuocTinhDacBietCtrl',function($http, $scope){
 			console.log("Erorr load all TTDB", error)
 		})
 	}
+	
+	//load thuộc tính đặc biệt theo id
+	$scope.load_by_id = function(maTtdb){
+		var url = `${host}/ThuocTinhDacBiet/${maTtdb}`
+		$http.get(url).then(resp => {
+			$scope.form = resp.data
+			$scope.update = true
+		}).catch(error => {
+			console.log("Error load TTDB by id", error)
+		})
+	}
+	
 	
 	//Tự động chạy đầu tiên khi chọn tab
 	$scope.load_all()
