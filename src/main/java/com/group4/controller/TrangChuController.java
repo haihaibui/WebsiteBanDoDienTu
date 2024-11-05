74ac526ae26bb2099beeca26ee317467692d2015
Hoang0207 <giuse02072003@gmail.com>
2024-11-05 18:04:33 +0700

    modify project

--- src/main/java/com/group4/controller/TrangChuController.java
+++ src/main/java/com/group4/controller/TrangChuController.java
@@ -1,28 +1,8 @@
 package com.group4.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.group4.dao.ChungLoaiDAO;
-import com.group4.dao.DonHangDAO;
-import com.group4.dao.NguoiDungDAO;
-import com.group4.dao.NhaCungCapDAO;
-import com.group4.dao.SanPhamDAO;
-import com.group4.dao.ThuocTinhDacBietDAO;
-import com.group4.entity.ChungLoai;
-import com.group4.entity.DonHang;
-import com.group4.entity.NguoiDung;
-import com.group4.entity.NhaCungCap;
-import com.group4.entity.SanPham;
-import com.group4.entity.ThuocTinhDacBiet;
-import com.group4.util.SessionUtil;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class TrangChuController {
@@ -33,4 +13,9 @@ public class TrangChuController {
 		return "redirect:/admin/html/QuanLyIndex.html";
 	}
 	
+	@GetMapping()
+	public String index(Model model) {
+		model.addAttribute("content","/layout/content2");
+		return "index";
+	}
 }
